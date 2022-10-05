80d204a8a171519a129eeecc23a50347f23748bb
James Walter <Panvather@gmail.com>
2022-10-05 21:55:11 +0530

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 const screen = document.querySelector('.drawing-board');
 
 let x = prompt('What should be the resolution: ');
+if (x > 512) {
+    x = prompt('Value is too large. It should be under 64: ')
+};
+
 let side = 512/x ;
 for (let i = 0; i < x**2; i++) {
     const box = document.createElement('div');
@@ -10,5 +14,15 @@ for (let i = 0; i < x**2; i++) {
     screen.appendChild(box);
 };
 //screen.addEventListener('mouseenter', () => console.log("Mouse entered"));
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => box.addEventListener('mouseenter', () => box.style.cssText = 'background-color: gold;'));
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(box => box.addEventListener('mouseover', changeColor)); 
+boxes.forEach(box => box.addEventListener('mousedown', changeColor));
+
+function changeColor(e) {
+    if (e.type === 'mouseover' ) {
+            e.target.style.background = "gold";
+    }
+}
+//box.addEventListener( box.style.background = 'gold');
+//boxes.forEach(box => box.addEventListener('mouseover', () => box.style.background = 'gold'));
